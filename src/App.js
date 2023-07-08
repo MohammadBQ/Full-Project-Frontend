c5e4635c38c2af7cf939212a61d96a69bf4304fb
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-07-08 15:32:17 +0300

    Add auth and categories

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./component/pages/Home";
 import Categories from "./component/pages/Categories";
 import Recipes from "./component/pages/Recipes";
+import Navbar from "./component/Navbar";
+
+import Signup from "./component/pages/Signup";
+import UserSignin from "./component/pages/UserSignin";
 
 function App() {
   return (
     <div>
-      <h1>Hello react</h1>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/recipes" element={<Recipes />} />
+        <Route path="/signin" element={<UserSignin />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+// import UserContext from "./context/UserContext";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  // const [user, setUser] = useContext(UserContext);
+
+  return (
+    <nav className="bg-orange-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link to="/">
+              <span className="font-semibold text-xl text-white">
+                A Nice Meal{" "}
+              </span>
+            </Link>
+          </div>
+          <div className="block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <NavLink
+                to="/"
+                className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/recipes"
+                className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Recipes
+              </NavLink>
+              {/* {user ? (
+                <button>signout</button>
+              ) : ( */}
+              <>
+                <NavLink
+                  to="/categories"
+                  className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Categories
+                </NavLink>
+                <NavLink
+                  to="/signup"
+                  className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Signup
+                </NavLink>
+                <NavLink
+                  to="/signin"
+                  className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Signin
+                </NavLink>
+              </>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
