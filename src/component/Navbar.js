7c534eb8eb90c92482678094520cdaa978f6af61
Najat76 <safari76@hotmail.co.uk>
2023-07-08 12:13:43 +0300

    work up to 12-12

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useContext } from "react";
+import { NavLink, useInRouterContext } from "react-router-dom";
 import ANM from "../../src/assets/images/ANM.png";
 import "../assets/css/style.css";
 import Home from "../component/Home";
 
 const Navbar = () => {
+  const [user, setUser] = useContext(UserContext);
+
+  const handleSignout = () => {
+    Signout();
+    setUser(false);
+  };
+
   return (
     <div className="bigBox">
       <div>
@@ -14,10 +21,34 @@ const Navbar = () => {
           </nav>
         </div>
         <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/Recipes">All Recipes</NavLink>
-          <NavLink to="/Shows">Shows</NavLink>
-          <NavLink to="/Chefs">Chefs</NavLink>
+          <NavLink to="/">
+            <div>Home</div>
+          </NavLink>
+          <NavLink to="/Recipes">
+            <div>All Recipes</div>
+          </NavLink>
+          <NavLink to="/Shows">
+            <div>Shows</div>
+          </NavLink>
+          <NavLink to="/Chefs">
+            <div>Chefs</div>
+          </NavLink>
+          {user ? (
+            <button onClick={handleSignout}>
+              <NavLink to="/sigin">
+                <div>sigout</div>
+              </NavLink>
+            </button>
+          ) : (
+            <div>
+              <NavLink to="/signin">
+                <div>signin</div>
+              </NavLink>
+              <NavLink>
+                <div>signup</div>
+              </NavLink>
+            </div>
+          )}
         </div>
       </div>
     </div>
