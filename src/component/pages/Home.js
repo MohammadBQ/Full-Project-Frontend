5c17d7bb3be11bf477a28a66db4b0d56a27029bd
Najat76 <safari76@hotmail.co.uk>
2023-07-09 19:46:55 +0300

    added from najat

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,11 +1,46 @@
 import React from "react";
+import BeefAndPepperStirFry from "../assets/images/BeefAndPepperStirFry.jpg";
+import Shakshuka from "../assets/images/Shakshuka.jpg";
+import EasySpaghettiBolognese from "../assets/images/EasySpaghettiBolognese.jpg";
+import AirFryerCrispyChickenBurger from "../assets/images/AirFryerCrispyChickenBurger.jpg";
+import BakedSweetPotato from "../assets/images/BakedSweetPotato.jpg";
+import ChickpeaFattoush from "../assets/images/ChickpeaFattoush.jpg";
 
 const Home = () => {
   return (
     <div>
-      Home
-      <h1>AAAA</h1>
-      <h2>Test 2</h2>
+      <div class="bg-image img1">
+        <img src={BeefAndPepperStirFry} alt="BeefAndPepperStirFryImage"></img>
+      </div>
+      <div class="bg-image img2">
+        <img src={Shakshuka} alt="ShakshukaImage"></img>
+      </div>
+      <div class="bg-image img3">
+        <img
+          src={EasySpaghettiBolognese}
+          alt="EasySpaghettiBologneseImage"
+        ></img>
+      </div>
+      <div class="bg-image img4">
+        <img
+          src={AirFryerCrispyChickenBurger}
+          alt="AirFryerCrispyChickenBurgerImage"
+        ></img>
+      </div>
+      <div class="bg-image img5">
+        <img src={BakedSweetPotato} alt="BakedSweetPotatoImage"></img>
+      </div>
+      <div class="bg-image img6">
+        <img src={ChickpeaFattoush} alt="ChickpeaFattoushImage"></img>
+      </div>
+      <br />
+      <div class="bg-text">
+        <h2>A NICE MEAL</h2>
+        <p>
+          The aim of this Application is to make meals as pleasurable to prepare
+          as they are to eat
+        </p>
+      </div>
     </div>
   );
 };
